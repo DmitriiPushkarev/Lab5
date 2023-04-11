58cc4ee7af63b875ca5d21b4a5d3f013bb276b27
PUSHKAREV DMITRII <dima_pushkarev@list.ru>
2023-04-11 20:42:53 +0500

    commit

---
+++ src/test/java/com/pushkarev/lab5/Lab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushkarev.lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
